ed15d7d8ae1e2dd26094197bd76ff3acd1ddded4
rumothy <rumothy@yahoo.com>
2020-09-21 20:08:28 -0400

    Adds basics

---
+++ mc/CodeAnalysis/SyntaxKind.cs
@@ -0,0 +1,20 @@
+namespace Minsk.CodeAnalysis
+{
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhitespaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        BadToken,
+        EndOfFileToken,
+        NumberExpression,
+        BinaryExpression,
+        ParenthesizedExpression
+    }
+
+}
